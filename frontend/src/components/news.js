55a3c4ef9f37802feec321a5773183bf4f666abb
Danieltdca <danieltdca@gmail.com>
2026-07-25 11:18:59 -0300

    Atualiza chamadas https com cloudflare

--- frontend/src/components/news.js
+++ frontend/src/components/news.js
@@ -13,7 +13,7 @@ function News() {
     try {
       setLoading(true);
       setError(null);
-      const response = await fetch("http://54.226.251.97/dominioni/backend/get-news.php");
+      const response = await fetch("https://randy-tires-theories-costs.trycloudflare.com/dominioni/backend/get-news.php");
       
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
